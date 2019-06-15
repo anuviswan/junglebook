16ed6af1435b1a86434516144a15fd186fe4aeb9
Anu Viswan <anu.viswan@gmail.com>
2019-06-15 09:05:56 +0530

    Adding images/flamingo.jpg

--- src/jungle_book/lib/main.dart
+++ src/jungle_book/lib/main.dart
@@ -1,37 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MainApplication());
 
-class MainApplication extends StatefulWidget {
-  @override
-  _MainApplicationState createState() => _MainApplicationState();
-}
+void main() => runApp(MyApp());
 
-class _MainApplicationState extends State<MainApplication> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Jungle Book',
       home: new Scaffold(
         appBar: new AppBar(title: new Text('Jungle Book')),
-        drawer: new ListView(
-          children: <Widget>[
-            DrawerHeader(
-                child: Text('Jungle Book'),
-                decoration: BoxDecoration(
-                  color: Colors.blue
-                ),),
-            new ListTile(
-              title: new Text('Random Mode'),
-              trailing: new Icon(Icons.autorenew),
-            ),
-            new ListTile(
-              title: new Text('Scene Mode'),
-              trailing: new Icon(Icons.picture_in_picture),
-            )
-          ],
-        ),
+        drawer: new AppMenu(),
+        body: new MainPageManager(),
       ),
+    );
+  }
+}
+
+class MainPageManager extends StatefulWidget {
+  @override
+  _MainPageManagerState createState() => _MainPageManagerState();
+}
+
+class _MainPageManagerState extends State<MainPageManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new Image.asset('images/flamingo.jpg',
+        fit: BoxFit.cover,)
+    );
+  }
+}
+
+
+class AppMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      children: <Widget>[
+        DrawerHeader(
+          child: Text('Jungle Book'),
+          decoration: BoxDecoration(
+              color: Colors.blue
+          ),),
+        new ListTile(
+          title: new Text('Random Mode'),
+          trailing: new Icon(Icons.autorenew),
+          onTap: () => Navigator.of(context).pop(),
+        ),
+        new ListTile(
+          title: new Text('Scene Mode'),
+          trailing: new Icon(Icons.picture_in_picture),
+          onTap: () => Navigator.of(context).pop(),
+        )
+      ],
     );
   }
 }
