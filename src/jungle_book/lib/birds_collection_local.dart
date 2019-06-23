9f019514625a9ecb63dc177e07f32f9c037aedf8
Anu Viswan <anu.viswan@gmail.com>
2019-06-23 21:02:50 +0530

    Working on using json as database catalogue instead of hard coded path

--- src/jungle_book/lib/birds_collection_local.dart
+++ src/jungle_book/lib/birds_collection_local.dart
@@ -1,12 +1,12 @@
 import 'package:jungle_book/fauna_meta_data.dart';
-
 import 'fauna_collection_base.dart';
+import 'dart:io';
 
 class BirdsLocalCollection extends FaunaCollectionBase
 {
   @override
   List<FaunaMetaData> GetList() {
-    // TODO: implement GetList
+    var file = File('data.txt');
     return null;
   }
 
--- src/jungle_book/lib/fauna_meta_data.dart
+++ src/jungle_book/lib/fauna_meta_data.dart
@@ -4,5 +4,12 @@ class FaunaMetaData
   String imageFilePath;
   String cryAudioFilePath;
   String pronunciationFilePath;
+
+  FaunaMetaData.fromJson(Map<String,dynamic> json){
+    name = json['name'];
+    imageFilePath = json['image'];
+    cryAudioFilePath = json['audio'];
+    pronunciationFilePath = json['pronunciation'];
+  }
 }
 
