5e1948c57fd0cee0c98d5028dc0171e5d9c53473
Anu Viswan <anu.viswan@gmail.com>
2019-06-24 19:38:04 +0530

    fixed path issue

--- src/jungle_book/lib/birds_collection_local.dart
+++ src/jungle_book/lib/birds_collection_local.dart
@@ -1,26 +1,16 @@
-import 'package:jungle_book/fauna_meta_data.dart';
 import 'fauna_collection_base.dart';
-import 'dart:io';
-import 'package:path/path.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class BirdsLocalCollection extends FaunaCollectionBase {
   @override
   Future<List<String>> GetList() async{
-    var data = await readFile('sds');
-    print(data);
-    print('soimething is wrong');
+    var data = await readFile();
     return [data];
   }
 
 
-  Future<String> readFile(String filePath) async {
-    print('reading data...');
-    new File(join(dirname(Platform.script.toFilePath()), '..', 'assets/database/birds.json')).readAsString().then((String contents)
-    {
-      print('data read');
-      print('contents=$contents');
-      return contents;
-    });
+  Future<String> readFile() async {
+    return await rootBundle.loadString('assets/database/birds.json');
   }
 }
 
