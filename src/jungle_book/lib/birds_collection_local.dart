e9a2342dd1131b0a322857429e1219f308621998
Anu Viswan <anu.viswan@gmail.com>
2019-06-25 21:14:21 +0530

    working on deserializing data

--- src/jungle_book/lib/birds_collection_local.dart
+++ src/jungle_book/lib/birds_collection_local.dart
@@ -1,10 +1,14 @@
 import 'fauna_collection_base.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'fauna_meta_data.dart';
+import 'dart:convert';
 
 class BirdsLocalCollection extends FaunaCollectionBase {
   @override
   Future<List<String>> GetList() async{
     var data = await readFile();
+    Map<String, dynamic> map = jsonDecode(data);
+    var result = FaunaMetaData.fromJson(map);
     return [data];
   }
 
