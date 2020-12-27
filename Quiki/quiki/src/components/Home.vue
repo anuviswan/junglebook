52030d91bca24ad816ba78ee6db2192ee999d17e
anu.viswan@gmail.com <njunni@1320>
2020-12-27 09:10:01 +0530

    Working on home

--- Quiki/quiki/src/components/Home.vue
+++ Quiki/quiki/src/components/Home.vue
@@ -1,31 +1,48 @@
 <template>
   <v-container fluid classfill-height>
-    <v-app-bar app dark color="primary darken-2" rounded="true">
-      <v-toolbar-title class="ml-2">
-        <div class="logo">
-          <i class="fa fa-superpowers fa-2x" aria-hidden="true"> Quiki</i>
-        </div>
-      </v-toolbar-title>
-    </v-app-bar>
+    <Navbar />
     <v-row>
-      <v-col></v-col>
+      <v-col>
+        <v-container fluid grid-list-md>
+          <v-layout row wrap>
+            <v-flex v-for="category in categories" :key="category" v-bind="xs6">
+              <v-card class="tile" color="#1976D2" dark justify="center">
+                <v-container fill-height fluid pa-2>
+                  <v-layout fill-height align-center>
+                    <v-flex xs12 align-end flexbox>
+                      <span v-text="category" class="text-uppercase"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import Navbar from "./generic/Navbar";
 export default {
   name: "Home",
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      categories: ["Birds", "Animals", "Flags"],
+    };
+  },
 };
 </script>
 
 <style scoped>
-.logo {
-  color: white;
-  font-family: "Eczar";
-  font-size: 1vw;
+.tile {
+  color: blue;
   font-weight: bold;
-  font-style: italic;
   text-align: center;
+  height: 15vw;
 }
 </style>
---
+++ Quiki/quiki/src/components/generic/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app dark color="primary darken-2" rounded="true">
+    <v-toolbar-title class="ml-2">
+      <div class="logo">
+        <i class="fa fa-superpowers fa-2x" aria-hidden="true"> Quiki</i>
+      </div>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.logo {
+  color: white;
+  font-family: "Eczar";
+  font-size: 1vw;
+  font-weight: bold;
+  font-style: italic;
+  text-align: center;
+}
+</style>
