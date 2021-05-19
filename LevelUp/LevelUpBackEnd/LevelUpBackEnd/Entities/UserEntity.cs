4684c1e045bc034952f436c6fe291ac99f5d9617
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-19 07:47:12 +0530

    Adding login api

---
+++ LevelUp/LevelUpBackEnd/LevelUpBackEnd/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace LevelUpBackEnd.Entities
+{
+    public class UserEntity : TableEntity
+    {
+        public string UserName { get; set; }
+        public int CurrentLevel { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ LevelUp/LevelUpBackEnd/LevelUpBackEnd/Helper/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpBackEnd.Helper
+{
+    public static class Utils
+    {
+        public const string Key_Partition = "Key";
+        public const string Key_User = "User";
+        public const string Key_CategoryRow = "Category";
+        public const string Key_ItemRow = "Value";
+        public const string Key_FileToken = "FileToken";
+
+        public const string FunctionName_GetNextQuestion = "GetNextQuestion";
+
+        public const string App_Parition = "AppMetadata";
+        public const string Category_Partition_Key_Suffix = "CategoryName";
+        public const string Table_Name = "levelUp";
+        public const string Blob_Container_Name = "genericimagestore";
+
+        public const string QueryString_AppId = "AppId";
+        public const string QueryString_CategoryId = "Cid";
+        public const string QueryString_AppName = "AppName";
+        public const string QueryString_DisplayName = "DisplayName";
+    }
+}
