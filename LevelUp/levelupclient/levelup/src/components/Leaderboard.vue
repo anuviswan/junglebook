70bf6f5e00ee65472518c74bd9478bcd0ff22fd0
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-19 16:18:32 +0530

    Adding Story.vue

---
+++ LevelUp/levelupclient/levelup/src/components/Leaderboard.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-row>
+        <v-col col="12">
+          <v-banner single-line color="blue" elevation="4" rounded dark
+            >LEADER BOARD</v-banner
+          >
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    User
+                  </th>
+                  <th class="text-left">
+                    Rank
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in participants" :key="item.name">
+                  <td class="text-left">{{ item.userName }}</td>
+                  <td class="text-left">{{ item.rank }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Leaderboard",
+  data() {
+    return {
+      participants: [
+        {
+          userName: "sreena",
+          rank: 1,
+        },
+        {
+          userName: "anu",
+          rank: 1,
+        },
+        {
+          userName: "jia",
+          rank: 1,
+        },
+        {
+          userName: "naina",
+          rank: 1,
+        },
+        {
+          userName: "manu",
+          rank: 1,
+        },
+        {
+          userName: "praji",
+          rank: 1,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ LevelUp/levelupclient/levelup/src/components/Story.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card color="grey lighten-2" class="font-italic">
+    The hunt for mytholigical treasure is about to begin.... In the year 1503, a
+    war broke out between the Kingdom of Zomorin(Samoothiri), Calicut and the
+    Kochi Raja. Despite having the upper hand, the Samoothiris, on hearing about
+    the arrival of a Portuguse fleet under Francisco and Afonso de Albuquerque,
+    had to retreat. Myths tells us that Samoothiris didn't take back the entire
+    treasure looted by them. Instead they hid half the treasure at different
+    places across the land that raised between Gokarana and Kanyakumari when
+    Parushuram threw his blood stained axe. And years later, the treasure still
+    lies undiscovered. Now its your chance to rediscover the treasure. Do you
+    have it in you to uncover the hidden clues and discover the great treasure
+    ?? Join the mordern day warrior's as they make their quest to unearth the
+    treasure.
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Story",
+};
+</script>
+
+<style></style>
