92394597be61e16d89e70feecc5c02ff53303b84
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-19 08:02:37 +0530

    Setting up base skeleton

--- LevelUp/levelupclient/levelup/src/main.js
+++ LevelUp/levelupclient/levelup/src/main.js
@@ -6,10 +6,10 @@ import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
-  router,
+  vuetify : new Vuetify(),  router,
   store,
-  vuetify : new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
