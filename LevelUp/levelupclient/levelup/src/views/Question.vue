46e422b47f0fee2418fa5e1d63fa10c71dc229c6
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-21 20:00:33 +0530

    Question page

--- LevelUp/levelupclient/levelup/src/views/Question.vue
+++ LevelUp/levelupclient/levelup/src/views/Question.vue
@@ -15,8 +15,40 @@
     <v-row>
       <v-col>
         <img
-          src="http://127.0.0.1:10000/devstoreaccount1/levelupimagestore/abc.jpg"
+          src="http://127.0.0.1:10000/devstoreaccount1/levelupimagestore/4260c34d-0fd4-4c66-a8e4-426828c0431d.jpg"
         />
+      </v-col>
+    </v-row>
+    <v-row
+      ><v-col>
+        <v-card>
+          <v-form v-model="valid" @submit="onSubmit">
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-banner single-line color="blue" elevation="4" rounded dark
+                    >Type your answer here</v-banner
+                  >
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="12">
+                  <v-text-field
+                    v-model="answer"
+                    :rules="nameRules"
+                    label="Answer"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  ><v-btn color="blue" type="submit" dark>Check Answer</v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -30,6 +62,7 @@ export default {
   data() {
     return {
       level: 1,
+      answer: "",
     };
   },
   computed: mapGetters(["currentUser"]),
