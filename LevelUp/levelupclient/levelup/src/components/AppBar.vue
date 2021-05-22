35dea4ac05064da2fd13b6d8780cd2214e5b9d9c
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-22 17:25:14 +0530

    rename title

--- LevelUp/levelupclient/levelup/src/components/AppBar.vue
+++ LevelUp/levelupclient/levelup/src/components/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-app-bar color="blue" dense dark>
-      <v-toolbar-title color="white">Nikathil Treasure Hunt </v-toolbar-title
+      <v-toolbar-title color="white">Family Treasure Hunt </v-toolbar-title
       ><v-spacer></v-spacer>{{ currentUser.userName }}
     </v-app-bar>
   </v-card>
