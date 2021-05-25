94ee6733e2c297ec1400fee7f58e05dd916075f3
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-25 05:48:53 +0530

    Added Winner notification

--- LevelUp/levelupclient/levelup/src/router/index.js
+++ LevelUp/levelupclient/levelup/src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Question from '../views/Question.vue'
 import Home from '../views/Home.vue'
 import Loading from "../views/Loading.vue"
-
+import Winner from "../views/Winner.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -21,6 +21,11 @@ const routes = [
     path: '/Loading',
     name: 'Loading',
     component: Loading
+  },
+  {
+    path:"/Winner",
+    name:"Winner",
+    component:Winner
   }
 ]
 
---
+++ LevelUp/levelupclient/levelup/src/views/Winner.vue
@@ -0,0 +1,36 @@
+<template>
+  <container>
+    <v-row>
+      <v-col>
+        <v-sheet
+          class="text-h2"
+          elevation="1"
+          rounded
+          color="orange accent-2"
+          dark
+          >CONGRATS !!
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet
+          class="text-h5"
+          elevation="1"
+          rounded
+          color="orange accent-2"
+          dark
+          >You just discovered the hidden treaure
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </container>
+</template>
+
+<script>
+export default {
+  name: "Winner",
+};
+</script>
+
+<style></style>
