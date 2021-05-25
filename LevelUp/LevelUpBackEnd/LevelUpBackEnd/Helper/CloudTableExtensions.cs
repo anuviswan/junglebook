442f97a78d1a22a977f43ab205ad9dff33049fdb
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-26 05:08:19 +0530

    Code Cleanup

--- LevelUp/LevelUpBackEnd/LevelUpBackEnd/Helper/CloudTableExtensions.cs
+++ LevelUp/LevelUpBackEnd/LevelUpBackEnd/Helper/CloudTableExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using LevelUpBackEnd.Entities;
 using Microsoft.Azure.Cosmos.Table;
 
--- LevelUp/LevelUpBackEnd/LevelUpBackEnd/Helper/Utils.cs
+++ LevelUp/LevelUpBackEnd/LevelUpBackEnd/Helper/Utils.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LevelUpBackEnd.Helper
+﻿namespace LevelUpBackEnd.Helper
 {
     public static class Utils
     {
         public const string Key_Partition = "Key";
         public const string Key_User = "User";
         public const string Key_Question = "Question";
-        public const string Key_ItemRow = "Value";
-        public const string Key_FileToken = "FileToken";
 
         public const string FunctionName_GetNextQuestion = "GetNextQuestion";
         public const string FunctionName_GetScores = "GetScores";
         public const string FunctionName_CreateQuestion = "CreateQuestion";
         public const string FunctionName_ValidateAnswer = "Validate";
 
-        public const string App_Parition = "AppMetadata";
-        public const string Category_Partition_Key_Suffix = "CategoryName";
         public const string Table_Name = "levelUp";
         public const string Blob_Container_Name = "levelupimagestore";
 
         public const string QueryString_AppId = "AppId";
-        public const string QueryString_CategoryId = "Cid";
         public const string QueryString_AppName = "AppName";
-        public const string QueryString_DisplayName = "DisplayName";
     }
 }
